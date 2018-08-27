d810fdab52b93295d3d084f9d16564c818a01989
Константин Шерыхалин <konstantin-sherykhalin@yandex.ru>
2018-08-27 18:23:29 +0500

    Созданы папки разделов

---
+++ src/components/enter/registration/index.js
@@ -0,0 +1,6 @@
+import React,{Component} from 'react';
+import {BrowserRouter,Route,Switch,Link} from 'react-router-dom';
+
+import Layout from './layout';
+
+export default () => <Layout/>;
--- src/components/user/layout.js
+++ src/components/user/layout.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-import logo from './logo.svg';
+import './style.css';
 
 export default (props) => (
 	<div className="app">
 		<header className="app_header">
-			<img className="app_logo" src={logo} />
 			<h1 className="app_title">Финополис 2018</h1>
-			<ul className="list">
-				<li>Роадмап</li>
-				<li>Команда</li>
-				<li>Финансы</li>
-				<li>Пилот</li>
-			</ul>
 		</header>
 		<div className="content">
 			<p>Здесь будет приложение.</p>
